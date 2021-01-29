0f6fd86ba2bdf9dd01cd1cc4c88877275fb5e6d8
aguo71 <yingzhe_guo@brown.edu>
2021-01-28 21:43:51 -0500

    6/6 system tests

--- src/main/java/edu/brown/cs/student/stars/Repl.java
+++ src/main/java/edu/brown/cs/student/stars/Repl.java
@@ -4,8 +4,12 @@ package edu.brown.cs.student.stars;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Repl {
     Map<String, Action> commands = new HashMap<>();
@@ -29,8 +33,18 @@ public class Repl {
             if(input == null) {
                 return;
             }
-            String delims = "[ ]+";
-            String[] tokens = input.split(delims);
+
+            // parses REPL command, splitting on spaces except within double quotes
+            // https://stackoverflow.com/questions/366202/regex-for-splitting-a-string-using-space-when-not-surrounded-by-single-or-double
+
+            List<String> matchList = new ArrayList<>();
+            Pattern regex = Pattern.compile("[^\\s\"']+|\"[^\"]*\"|'[^']*'");
+            Matcher regexMatcher = regex.matcher(input);
+            while (regexMatcher.find()) {
+                matchList.add(regexMatcher.group());
+            }
+            String[] tokens = new String[matchList.size()];
+            tokens = matchList.toArray(tokens);
 
             if(commands.containsKey(tokens[0])) {
                 commands.get(tokens[0]).run(tokens);
